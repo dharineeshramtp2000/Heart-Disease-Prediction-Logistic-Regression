281d4fa78aecc8045667738dc03a75cfd4d69aa8
Dharineesh Ram T P <60029791+dharineeshramtp2000@users.noreply.github.com>
2020-05-08 19:16:30 +0530

    Python

--- Heart Disease.py
+++ Heart Disease.py
@@ -1,24 +1,31 @@
+#Import the Libraries
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+#Importing the Dataset. Here we use the dataset from Kaggle.
 Dataset = pd.read_csv("heart_disease_dataset.csv")
 X = Dataset.iloc[:, :-1].values
 y = Dataset.iloc[:, -1].values
 
+#Now lets feature scale our independent variables from our famous scikit library
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X = sc_X.fit_transform(X)
 
+#Splitting the dataset into training and testing and performing a good shuffle of data
 from sklearn.model_selection import train_test_split
 X_train , X_test, y_train, y_test = train_test_split(X, y, test_size= 0.3, random_state=42, shuffle=True)
 
+#Now lets define our Logistic Regression model. 
 from sklearn.linear_model import LogisticRegression
-classifier = LogisticRegression(max_iter=1000, random_state = 0, multi_class='ovr', solver='lbfgs')
+classifier = LogisticRegression(max_iter=1000, random_state = 0, multi_class='ovr', solver='sag')
 classifier.fit(X_train, y_train)
 
+#Now lets predict with our test set
 y_pred = classifier.predict(X_test)
 
+#Its time for evaluating our model.
 from sklearn.metrics import confusion_matrix, accuracy_score, f1_score
 acc_train = accuracy_score(y_train, classifier.predict(X_train))
 f1_train = f1_score(y_train, classifier.predict(X_train), average= 'weighted')
